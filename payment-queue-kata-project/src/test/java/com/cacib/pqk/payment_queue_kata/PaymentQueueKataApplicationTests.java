06f41a0e44c5149b929ed5f6544a0d2737913104
“Clems” <c.gibertpro@gmail.com>
2025-04-04 09:32:33 +0200

    Init Spring boot and dependencies and gitignore

---
+++ payment-queue-kata-project/src/test/java/com/cacib/pqk/payment_queue_kata/PaymentQueueKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cacib.pqk.payment_queue_kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentQueueKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
